03365882fef3fd7feaf645f46fbcd4b5655f5e56
Pavel Kazachenko <pkazachenkoo@gmail.com>
2024-07-16 19:44:35 +0300

    init

---
+++ server-logic/src/main/java/com/mxrph/emergent/EmergentApplication.java
@@ -0,0 +1,13 @@
+package com.mxrph.emergent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergentApplication.class, args);
+	}
+
+}
